f8779abbc5eb9d9abaae5d50d37ac53d49d8813a
Your Name <you@example.com>
2019-10-11 12:42:05 +0700

    main_last

--- bot.py
+++ bot.py
@@ -23,10 +23,13 @@ def send_echo(message):
     temp = w.get_temperature('celsius')['temp']
     answer = "В городе " + place + " сейчас " + w.get_detailed_status() + ', температура ' + str(temp) + '°C' + '\n'
 
-    if temp < 10:
-        answer += 'Сейчас холодно одевайся теплее' + '\n'
+    if temp < 0:
+        answer += 'Ура! Скоро в прорубь' + '\n\n'
+    elif temp < 10:
+        answer += 'Прохладно, куртку не забудь!' + '\n\n'
     elif temp < 20:
-        answer += 'Сейчас тепло зачем лишняя одежда?' + '\n'
+        answer += 'Да похоже лето скоро!' + '\n\n'
+
 
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -61,7 +64,7 @@ def send_echo(message):
         answer += 'Событий к сожалению нет'+ '\n'
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        answer += start + ' ' + event['summary'] + ' ' + event['description'] + '\n'
+        answer += start + ' ' + event['summary'] + ' ' + event['description'] + '\n\n'
 
     bot.send_message(message.chat.id, answer)
 # @bot.message_handler(func=lambda message: True)
